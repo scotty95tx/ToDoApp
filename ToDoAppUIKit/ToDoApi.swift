5a25f8a643e1135c9e246ef8743a8b967bc20e8b
Mikaela Caron <mikaelacaron@gmail.com>
2022-04-03 11:59:01 -0400

    Initial Suggestions

--- ToDoAppUIKit/ToDoApi.swift
+++ ToDoAppUIKit/ToDoApi.swift
@@ -13,6 +13,7 @@ final class ToDoListAPI {
     
     func fetchToDoList(onCompletion: @escaping ([ToDo]) -> ()) {
         let urlString = "https://jsonplaceholder.typicode.com/todos"
+        // don't force unwrap, instead use a guard and throw an error. If this were to fail, the app would crash.
         let url = URL(string: urlString)!
         
         let task = URLSession.shared.dataTask(with: url) { (data, resp, error) in
@@ -34,6 +35,7 @@ final class ToDoListAPI {
     }
 }
 
+// put this in a separate file
 struct ToDo: Codable {
     let userId: Int
     let id: Int
